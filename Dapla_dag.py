f9ed3e4d025097ae2473b3ccd71168319bfcf7c7
Alex Mellbye <82661985+Alex-Mellbye@users.noreply.github.com>
2023-06-27 12:42:49 +0200

    Create Dapla_dag.py

---
+++ Dapla_dag.py
@@ -0,0 +1,19 @@
+
+
+
+
+# Test for GitHub
+
+# Her skriver man kode som man laster inn og kjører i en Jupyter Notebook
+# Dette kan være kode skrevet i Python, R, C++, etc (men ikke SAS fordi SAS er lisensdrevet og ikke open-source som de andre språkene)
+
+
+# God Dapla-dag Krimgruppa!
+
+
+Dapla = "Krimgruppa "
+Dag = "rocker!"
+Dato = "28.06.2040"
+
+print(Dapla+Dag+" Og datoen i dag er "+Dato)
+
